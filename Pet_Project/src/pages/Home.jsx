912e14252e515ebe276968096f31687ed1467c50
Jack <jackmadden13@hotmail.co.uk>
2023-05-24 21:51:03 +0100

    Updated components with fake backend data

--- Pet_Project/src/pages/Home.jsx
+++ Pet_Project/src/pages/Home.jsx
@@ -1,19 +1,14 @@
 import WelcomeMessage from "../components/Welcome";
 import DogDescription from "../components/DogDescription";
-import { welcomeText } from "../components/BackendData";
 import CharlieImage from "../assets/Charlie.jpg";
 import PorshaImage from "../assets/Porsha-puppy.jpg";
 import RosyImage from "../assets/Rosy.jpg";
 
 const HomePage = () => {
-  const welcomeMessage = welcomeText.map(({w}) => w.title, w.text, w.details )
+  
 return (
     <> 
-    <WelcomeMessage
-    title='Pet Sanctuary Project' 
-    text='This is a project dedicated to showcasing beautiful animals that have had tragic lives, and would be very appreciative of a loving home.' 
-    details='Feel free to click on a pet below and find out their story:' 
-  />
+    <WelcomeMessage />
   <div style = {{textAlign: "center"}}> 
     <DogDescription
     link = "/dogs/Charlie"
